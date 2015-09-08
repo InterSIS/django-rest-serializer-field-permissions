65e2bc300aad55cc9dd33135d28e030f8888d150
Joseph Schilz <joseph@schilz.org>
2015-09-07 21:53:00 -0700

    Semantic versioning.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-rest-serializer-field-permissions',
-    version='0.34',
+    version='1.0.0',
     packages=['rest_framework_serializer_field_permissions'],
     include_package_data=True,
     license='GNU General Public License v3 (GPLv3)',
